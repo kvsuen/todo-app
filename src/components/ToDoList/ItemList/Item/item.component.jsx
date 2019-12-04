94b3d4dba3ead8f0d0d08867090b8004af3f943a
Kevin Suen <kevinsuen11@gmail.com>
2019-12-03 19:05:29 -0500

    Implement edit todo item

--- src/components/ToDoList/ItemList/Item/item.component.jsx
+++ src/components/ToDoList/ItemList/Item/item.component.jsx
@@ -2,24 +2,92 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { format } from 'date-fns';
 
-import { deleteItem } from '../../../../redux/actions/itemActions'
+import {
+  toggleComplete,
+  deleteItem
+} from '../../../../redux/actions/itemActions';
+
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 
-const Item = ({ deleteItem, id, title, description, dueDate, isCompleted }) => {
+const useStyles = makeStyles(theme => ({
+  expansionPanel: {
+    width: '100%'
+  },
+  heading: {
+    flex: 1,
+    margin: 'auto 15px'
+  },
+  dueDate: {
+    margin: 'auto 15px'
+  },
+  expansionPanelSummary: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+}));
+
+const Item = ({
+  toggleComplete,
+  deleteItem,
+  id,
+  title,
+  description,
+  dueDate,
+  isCompleted,
+  handleOpen
+}) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-      {format(dueDate, 'yyyy-MM-dd')}
-      {isCompleted ? 'Completed' : 'Pending'}
-      <DeleteIcon onClick={() => deleteItem(id)}/>
+    <div className={classes.expansionPanel}>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          classes={{ content: classes.expansionPanelSummary }}
+        >
+          {isCompleted ? (
+            <CheckBoxIcon onClick={() => toggleComplete(id)} />
+          ) : (
+            <CheckBoxOutlineBlankIcon onClick={() => toggleComplete(id)} />
+          )}
+
+          <Typography className={classes.heading} variant="h6">
+            {title}
+          </Typography>
+          <Typography className={classes.dueDate}>
+            Complete by: {format(dueDate, 'yyyy-MM-dd')}
+          </Typography>
+          <div>
+            <EditIcon
+              onClick={() =>
+                handleOpen({ id, title, description, dueDate, isCompleted })
+              }
+            />
+            <DeleteIcon onClick={() => deleteItem(id)} />
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>{description}</Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </div>
   );
 };
 
 const mapDispatchToProps = {
+  toggleComplete,
   deleteItem
-}
+};
 
 export default connect(null, mapDispatchToProps)(Item);
--- src/components/ToDoList/ItemList/item-list.component.jsx
+++ src/components/ToDoList/ItemList/item-list.component.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Item from './Item/item.component';
+import AddEditModal from '../AddEditModal/add-edit-modal.component';
 
-const ItemList = ({ toDos }) => {
-  const ids = Object.keys(toDos)
+import { editItem } from '../../../redux/actions/itemActions';
+
+const ItemList = ({ toDos, editItem }) => {
+  const [open, setOpen] = useState(false);
+  const [initialState, setInitialState] = useState({
+    title: '',
+    description: '',
+    dueDate: new Date(),
+    isCompleted: false
+  });
+
+  const handleOpen = values => {
+    setInitialState({
+      id: values.id,
+      title: values.title,
+      description: values.description,
+      dueDate: values.dueDate,
+      isCompleted: values.isCompleted
+    });
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const ids = Object.keys(toDos);
   const items = ids.map(elem => {
     const { id, title, description, dueDate, isCompleted } = toDos[elem];
 
@@ -15,14 +41,22 @@ const ItemList = ({ toDos }) => {
         description={description}
         dueDate={dueDate}
         isCompleted={isCompleted}
+        handleOpen={handleOpen}
       />
     );
   });
 
   return (
-    <div style={{width: '100%'}}>
-      {items}
-    </div>
+    <>
+      <div style={{ width: '100%' }}>{items}</div>
+      <AddEditModal
+        open={open}
+        handleClose={handleClose}
+        initialState={initialState}
+        submit={editItem}
+        edit={true}
+      />
+    </>
   );
 };
 
@@ -32,4 +66,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(ItemList);
+const mapDispatchToProps = {
+  editItem
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
